5e9fe4e5a7bf2bbb9df238c8e4c6683b6f9a2e0f
DFL <s185120@win.dtu.dk>
2018-11-13 15:10:01 +0100

    Pusher ParticipantManageren

---
+++ src/ParticipantManager.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ParticipantManager {
+
+    private ArrayList<Participant> participant = new ArrayList();
+
+    public addParticipant(new Participant()) {
+
+
+        return participant;
+    }
+
+
+
+
+}
