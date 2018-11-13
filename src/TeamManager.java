fdc7b45131412b734cffc6fd77d229aeb5ee5b28
maxmojito <m@hotmail.com>
2018-11-13 15:18:45 +0100

    LoginController first draft

---
+++ src/TeamManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+//Max Gammelgaard
+public class TeamManager {
+    private ArrayList<Team> Team = new Arraylist();
+
+    public void addTeam(){
+    }
+
+    public void removeTeam(){
+    }
+}
