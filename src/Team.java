aefcb15952d56d3a2b67300a8a76c2631c454fe1
AHW1307 <Anders@miconsult.dk>
2018-11-13 14:52:56 +0100

    updated Team Class

--- src/Team.java
+++ src/Team.java
@@ -1,3 +1,12 @@
 //Anders
+
+import java.util.ArrayList;
+
 public class Team {
+
+    private ArrayList <Participants> participant = new ArrayList();
+
+    private static void addParticipant(){
+
+    }
 }
